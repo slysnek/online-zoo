03df059bfa84f9c06e30562f1ba4195c605db73f
Kirill <bloodwingest@gmail.com>
2022-10-10 20:20:38 +0300

    feat: added burger-menu for landing

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,1 +1,80 @@
 //alert('Привет! Если возникли вопросы в ходе проверки или что-то сломалось по непонятной причине - не стесняйся написать в дискорд Cpt.Bloodwing#2351 или в телеграм @screwpassenger')
+
+/*burger*/
+const burger = document.querySelector('.burger-menu');
+const header = document.querySelector('header')
+const cross = document.querySelector('.cross')
+const nav = document.querySelector('nav')
+const navbar = document.querySelector('.navbar')
+const logo = document.querySelector('.header-logo')
+const logoText = document.querySelector('.header-logo-text')
+const logoBamboo = document.querySelector('.logo-bamboo')
+const aboutNavbar = document.querySelector('.about-navbar')
+const designedBy = document.querySelector('.designed-by')
+const popUp = document.querySelector('.pop-up-background')
+let toggle = false;
+
+burger.addEventListener('click', () => {
+    header.classList.toggle('header-toggle')
+    burger.classList.toggle('cross')
+    nav.classList.toggle('visible')
+    navbar.classList.toggle('nav-toggle')
+    logo.classList.toggle('header-logo-toggle')
+    logoText.classList.toggle('header-logo-text-toggle')
+    logoBamboo.classList.toggle('logo-bamboo-toggle')
+    popUp.classList.toggle('visible')
+    aboutNavbar.classList.toggle('about-navbar-toggle')
+    designedBy.classList.toggle('visible')
+    designedBy.classList.toggle('designed-by-toggle')
+    if(toggle === false){
+        burger.src = "../../assets/icons/nav-cross.svg"
+        aboutNavbar.innerHTML = '<a href="./index.html">About</a>'
+        toggle = true
+    } else {
+        burger.src = "../../assets/icons/burger_menu.svg"
+        aboutNavbar.innerHTML = 'About'
+        toggle = false
+    }
+})
+
+popUp.addEventListener('click', () => {
+    header.classList.toggle('header-toggle')
+    burger.classList.toggle('cross')
+    nav.classList.toggle('visible')
+    navbar.classList.toggle('nav-toggle')
+    logo.classList.toggle('header-logo-toggle')
+    logoText.classList.toggle('header-logo-text-toggle')
+    logoBamboo.classList.toggle('logo-bamboo-toggle')
+    popUp.classList.toggle('visible')
+    aboutNavbar.classList.toggle('about-navbar-toggle')
+    designedBy.classList.toggle('visible')
+    designedBy.classList.toggle('designed-by-toggle')
+    if(toggle === false){
+        burger.src = "../../assets/icons/nav-cross.svg"
+        aboutNavbar.innerHTML = '<a href="./index.html">About</a>'
+        toggle = true
+    } else {
+        burger.src = "../../assets/icons/burger_menu.svg"
+        aboutNavbar.innerHTML = 'About'
+        toggle = false
+    }
+})
+
+window.addEventListener('resize', ()=> {
+    if(toggle === true){
+        header.classList.toggle('header-toggle')
+        burger.classList.toggle('cross')
+        nav.classList.toggle('visible')
+        navbar.classList.toggle('nav-toggle')
+        logo.classList.toggle('header-logo-toggle')
+        logoText.classList.toggle('header-logo-text-toggle')
+        logoBamboo.classList.toggle('logo-bamboo-toggle')
+        popUp.classList.toggle('visible')
+        aboutNavbar.classList.toggle('about-navbar-toggle')
+        designedBy.classList.toggle('visible')
+        designedBy.classList.toggle('designed-by-toggle')
+        burger.src = "../../assets/icons/burger_menu.svg"
+        aboutNavbar.innerHTML = 'About'
+        toggle = false
+    }
+})
